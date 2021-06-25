ad0148f71044a1346e40b47d07ec76ee1381c897
Roge <orogelioangel@gmail.com>
2021-06-25 17:31:25 -0500

    First upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php include('includes/head.php'); ?>
+
+<div class="">
+    <div class="jumbotron">
+        <h1 class="display-3">Projekt Ambient</h1>
+        <p class="lead">Bienvenido a la página oficial de nuestro proyecto!</p>
+        <hr class="my-4">
+        <p>Podras encontrar información navegando en las pestañas superiores</p>
+        <p class="lead">
+            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </p>
+    </div>
+</div>
+
+<hr>
+<div class="info">
+
+    <h2>MEDIO AMBIENTE</h2>
+    <p>El medio ambiente es el espacio en el que se desarrolla la vida de los organismos y que permite su interacción. Está conformado por seres vivos (factores bióticos), por elementos sin vida (factores abióticos) y por elementos artificiales creados por el hombre.
+        Se trata del entorno que condiciona la forma de vida de la sociedad y que incluye valores naturales, sociales y culturales que existen en un lugar y momento determinado.</a></p>
+    <p><small>Fuente: <a href="https://concepto.de/medio-ambiente/#ixzz6xQKnTDaW">Concepto medio ambiente</a></small>
+</div>
+
+<div class="info">
+
+    <h2>EL PROBLEMA</h2>
+    <p>Principalmente partimos a definir las condiciones geográficas del lugar donde ocurre
+el problema. En Cortázar el clima es semicálido húmedo con lluvias en verano,
+crecen diversas especies forrajeras, tales como navajita, zacatón, pata de gallo,
+flechilla, tres barbas, lanudo y tempranero. También hay huizaches, nopales y
+largoncillo. El mezquite es una especie muy común en toda la región. El suelo tiene
+una estructura blocosa angular y consistencia muy firme a firme, con textura de
+arcillo limosa a arcillo arenosa y pH de 6.8 a 8.9 de origen aluvión coluvial. El 72,16
+% de la superficie municipal se destina a actividades de carácter agrícola, el 5,16 %
+a pastizales; el 0,21 % a bosques y el 23 % a matorrales. Las principales fuentes de
+contaminación son el vertido de aguas residuales en el Río Laja y Canal (Dren)
+Merino generando contaminación al medio ambiente por agroquímicos y aguas
+residuales que son vertidas sin tratamiento previo, además esta agua se utiliza para
+riego, añadiendo la contaminación del aire por agroquímicos y quema de la pata de
+sorgo afectando además la visibilidad de los conductores en las vías de
+comunicación.</p>
+<p><small>Fuente: <a href="https://es.wikipedia.org/wiki/Municipio_de_Cortazar">Municipio Cortázar</a></small>
+<p><small>Fuente: <a href="https://portalsocial.guanajuato.gob.mx/sites/default/files/documentos/2003_CORTAZAR_Plan_de_ordenamiento_municipal.pdf">Ordenamiento Territorial</a></small>
+
+</div>
+
+
+<?php include('includes/footer.php'); ?>
